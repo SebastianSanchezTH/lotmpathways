872660510b491c49932f273c936b41a9f141ce56
SebastianSanchezTH <juan458.205@gmail.com>
2026-01-21 22:41:03 -0500

    Carousel and first UI change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React from "react";
 import { useState } from "react";
+import useEmblaCarousel from "embla-carousel-react";
 import "./styles/App.css";
 
 const quantity = 22;
@@ -11,42 +13,58 @@ const iconsColor = Array.from({ length: quantity }, (_, i) =>
   require(`./assets/iconColor/${i}_Color.png`),
 );
 
-function App() {
+export default function App() {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
   const [hovered, setHovered] = useState(null);
 
+  const scrollPrev = () => {
+  if (emblaApi) emblaApi.scrollPrev();
+};
+
+const scrollNext = () => {
+  if (emblaApi) emblaApi.scrollNext();
+};
+
   return (
-    <div>
-      <header className="App-header">
-        <div className="banner">
-          <div className="slider" style={{ "--quantity": quantity }}>
+    <div className="background">
+      <div className="embla">
+        <button className="embla__arrow embla__arrow--prev" onClick={scrollPrev}>&lt;</button>
+        <button className="embla__arrow embla__arrow--next" onClick={scrollNext}>&gt;</button>
+        <div className="world"></div>
+
+        <div className="embla__viewport" ref={emblaRef}>
+          <div className="embla__container">
             {iconsOff.map((off, i) => (
-              <div
-                className="item"
-                key={i}
-                style={{ "--i": i }}
-                onMouseEnter={() => setHovered(i)}
-                onMouseLeave={() => setHovered(null)}
-              >
-                <img
-                  src={hovered === i ? iconsColor[i] : off}
-                  alt={`icon${i}`}
-                  draggable="false"
-                />
+              <div className="embla__slide" key={i}>
+                  <div
+                    className="item"
+                    key={i}
+                    style={{ "--i": i }}
+                    onMouseEnter={() => setHovered(i)}
+                    onMouseLeave={() => setHovered(null)}
+                  >
+                    <img
+                      src={hovered === i ? iconsColor[i] : off}
+                      alt={`icon${i}`}
+                      draggable="false"
+                    />
+                  </div>
               </div>
-            ))}
+              ))}
+              
           </div>
-          <div className="world"></div>
+          
         </div>
 
-        <div className="content">
-          <h1>Lord of Mysteries</h1>
-          <div className="title">
-            <h2>The Blasphemy Project</h2>
-          </div>
+        <div className="main-title">
+          <h1 data-content="Lord of Mysteries">Lord of The Mysteries</h1>
         </div>
-      </header>
+        <div className="title">
+          <h2 data-content="The Blasphemy Project">The Blasphemy Project</h2>
+        </div>
+        
+      </div>
+      
     </div>
   );
 }
-
-export default App;
